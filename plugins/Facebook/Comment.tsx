800749506ba662d10ad1dae9f2cba8c1c0919d78
DESKTOP-CHT52UJ\ADMIN <winhieu2000@gmail.com>
2022-12-10 21:59:37 +0700

    feature like and share

--- plugins/Facebook/Comment.tsx
+++ plugins/Facebook/Comment.tsx
@@ -9,42 +9,21 @@ interface Comments {
   width?: string
 }
 const Comments = ({language, dataHref, numberPost, width}: Comments) => {
-  const initFacebookSDK = () => {
+  const initComments = () => {
     // if (window.FB) {
     //   window.FB.XFBML.parse()
     // }
     const facebookScript = document.createElement('script')
     facebookScript.async = true
-    facebookScript.src = `https://connect.facebook.net/en_US/sdk.js#xfbml=true&version=v2.7&cookie=true&status=true&appId=${process.env.FACEBOOK_APP_ID}&autoLogAppEvents=1`
+    facebookScript.defer = true
+    facebookScript.crossOrigin = 'anonymous'
+    // facebookScript.nonce = 'ugC4mrP6'
+    facebookScript.src = `https://connect.facebook.net/vi_VN/sdk.js#xfbml=true&version=v15.0&cookie=true&status=true&appId=${process.env.FACEBOOK_APP_ID}&autoLogAppEvents=1`
     document.body.appendChild(facebookScript)
-
-    // let locale = language ? language : 'en_US'
-    // window.fbAsyncInit = function () {
-    //   window.FB.init({
-    //     appId: process.env.FACEBOOK_APP_ID,
-    //     status: true,
-    //     cookie: true,
-    //     xfbml: true,
-    //     version: 'v2.7', // or v2.6, v2.5, v2.4, v2.3
-    //   })
-    // }
-    // // Load the SDK asynchronously
-    // ;(function (d, s, id) {
-    //   var js,
-    //     fjs = d.getElementsByTagName(s)[0]
-    //   if (d.getElementById(id)) {
-    //     return
-    //   }
-    //   js = d.createElement(s)
-    //   js.id = id
-    //   js.src = '//connect.facebook.net/' + locale + '/sdk.js'
-    //   fjs.parentNode.insertBefore(js, fjs)
-    // })(document, 'script', 'facebook-jssdk')
   }
 
   useEffect(() => {
-    console.log('dataHref',dataHref)
-    initFacebookSDK()
+    initComments()
   }, [])
 
   return (
@@ -55,6 +34,7 @@ const Comments = ({language, dataHref, numberPost, width}: Comments) => {
         data-href={dataHref}
         data-width={width ? width : ''}
         data-numposts={numberPost ? numberPost : 5}
+        data-mobile
       />
     </>
   )
